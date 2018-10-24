02471bc99ae75a143324691251a534a1980ba5ee
Isabel McKay <bellamckay@Isabels-MacBook-Air.local>
2018-10-24 08:19:56 -0700

    Homework

--- Lab1022WordSort/sketch.js
+++ Lab1022WordSort/sketch.js
@@ -1,51 +1,39 @@
-//  Global variables
-var boids = [];
-var chaser;
-
 // setup code here
+var txt;
+
+function preload(){
+  // p5 function named LoadStrings
+  txt = loadStrings("words.txt");
+}
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(0, 0, 0);
-  loadBoids(100);
-  frameRate(60);// !!!!!!!!!! this is a function call
-  chaser = new Ball(createVector(width/2, height/2), createVector(1,2), 25, color(255,0,0));
+myBubbleSort(txt);
 }
 
-// run chaser
-function draw() {
-  background(20,20,20,80);
-  chaser.run();
-  for (var i = 0; i < boids.length; i++){
-    boids[i].run();
-  }
+// fill in tomorrow with help
+function myBubbleSort(){
+  // maybe the started code?
+  for(var i = nums.length - 1; i >= 1; i-- ){
+    for(var j = 0; j < nums.length; j++ ){
 
-  // splice function to get rid of boids
-  for (var i = 0; i < boids.length; i++){
-    var x= boids[i];
-    var y = chaser.loc;
-    var z = x.loc;
-    // distance between 2 obj.
-    var dist = y.dist(z);
-    if (dist < 10)
-      boids.splice(i, 1);// remove boids
-    }
-  }
-
-
-
-
-
-
- // creating boids
-function loadBoids(numBoid){
-  for (var i = 50; i < numBoid; i++){
-    //varb = new Boid (loc, vel, dir, clr);
-    var loc = createVector(random(width),random(height));
-    var vel = createVector(random(-3,3), random(-3,3));
-    var rad = random(10,10);
-    var col = color(random(150,250),random(150,250),random(150,250));
-    boids.push(new Boid(loc, vel, rad, col));
-  }
-  console.log(boids);
 }
+
+
+}
+// or this?
+    swapped = false
+    for
+    if  then
+    swap
+    swapped = true
+    if
+    for
+
+// what is going on
+function swap(nums, a, b){
+   var temp = nums[a];
+   nums[a] = nums[b];
+   nums[b] = temp
+)
