3b84fcb97348fb0eb271eab63800e36adc24efb6
isabelmckay <43447413+isabelmckay@users.noreply.github.com>
2018-10-29 08:43:39 -0700

    classwork

--- Lab1026 ColorBars/colorbars.js
+++ Lab1026 ColorBars/colorbars.js
@@ -4,6 +4,10 @@
 ** Aug 21, 2018
 */
 
+myBubbleSort()]
+
+mySelectSort()
+
 function Ball(location, velocity, radius, col){
   // Instance Variables
   this.loc = location;
@@ -11,25 +15,3 @@ function Ball(location, velocity, radius, col){
   this.rad = radius;
   this.col = col;
   this.acc = createVector(0,.1);
-
-  // calls other functions
-  this.run = function(){
-    this.checkEdges();
-    this.render();
-    var mouseLoc = createVector(mouseX, mouseY);
-    this.loc = p5.Vector.lerp(this.loc, mouseLoc, .09);
-  }
-
-  //checkEdges() reverses speed when the ball touches an edge
-  this.checkEdges = function(){
-    if(this.loc.x < 0) this.vel.x = -this.vel.x
-    if(this.loc.x > width) this.vel.x = -this.vel.x
-    if(this.loc.y < 0) this.vel.y = -this.vel.y
-    if(this.loc.y > height) this.vel.y = -this.vel.y
-  }
-  // render () draws ball at the new location
-  this.render = function (){
-    fill(this.col);
-    ellipse(this.loc.x,this.loc.y, this.rad, this.rad)
-  }
-}
