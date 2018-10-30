7c279abb14e73bd72fb568313b92623075646289
Isabel McKay <bellamckay@Isabels-MacBook-Air.local>
2018-10-30 08:00:53 -0700

    homework

--- Lab1026 ColorBars/colorbars.js
+++ Lab1026 ColorBars/colorbars.js
@@ -4,14 +4,21 @@
 ** Aug 21, 2018
 */
 
+
 myBubbleSort()]
+console.log("after bubble sorting");
+console.log(txt);
+}
 
 mySelectSort()
 
-function Ball(location, velocity, radius, col){
+
+
+for(var i = 0; i <= 255; i++) { // fill strokes
+var color = 'rgb(100, ' + i + ', ' + i + ')';
+function Bar(location, velocity, radius, col){
   // Instance Variables
   this.loc = location;
   this.vel = velocity;
   this.rad = radius;
   this.col = col;
-  this.acc = createVector(0,.1);
--- Lab1026 ColorBars/sketch.js
+++ Lab1026 ColorBars/sketch.js
@@ -1,1 +1,15 @@
 // setup code here
+myBubbleSort()]
+
+mySelectSort()
+
+
+function myBubbleSort(){
+  for(var i = 0; i <= 255; i++) { // fill strokes
+    for(var j = 0; j < bar.length; j++ ){
+      // defining i and j
+      right_element = [i];
+      left_element = [j];
+      // if statement states when they should swap
+if(right_element< left_element){
+  // calling swap function
