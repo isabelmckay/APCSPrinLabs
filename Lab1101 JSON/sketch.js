da98237692ed0d13ccd34540c1fdaf2d8eb31326
isabelmckay <43447413+isabelmckay@users.noreply.github.com>
2018-11-06 08:42:12 -0800

    class

--- Lab1101 JSON/sketch.js
+++ Lab1101 JSON/sketch.js
@@ -3,21 +3,31 @@ var data;
 
 //setup code
 function preload(){
-  data = loadJSON("earthquake.geojson");
+  data = loadJSON("population.json");
 //  console.log(data);
 
 }
-function bubbleSort(){
-  for(var i = (data.length - 1); i >= 0; i--){
-    for(var j = 0; j < i; j++){
-      if(data[j].high < data[j+1].high){
-        var temp = data[j]
-        data[j+1] = // TEMP: }
+
+  function setup(){
+  noCanvas();
+  console.log(data.population)gotData;
+}
+function gotData(jData){
+   data = jData;
+}
+function draw(){
+  if(data);
+  function bubbleSort(){
+    for(var i = (data.length - 1); i >= 0; i--){
+      for(var j = 0; j < i; j++){
+        if(data[j].high < data[j+1].high){
+          var temp = data[j]
+          data[j+1] = // TEMP: }
+     swap(j, j + 1)
+   }
+
+        }
       }
     }
   }
-  function setup(){
-  noCanvas();
-  console.log(data.earthquake);
 }
- function draw(){
