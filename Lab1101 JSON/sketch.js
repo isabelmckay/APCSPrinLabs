700c29b70b2fa3241a9ca957e0d71acbf5f2d21d
isabelmckay <43447413+isabelmckay@users.noreply.github.com>
2018-11-08 08:30:38 -0800

    class

--- Lab1101 JSON/sketch.js
+++ Lab1101 JSON/sketch.js
@@ -1,33 +1,33 @@
 // global variables
-var data;
+var data = [];
 
 //setup code
 function preload(){
   data = loadJSON("population.json");
 //  console.log(data);
-
 }
 
   function setup(){
   noCanvas();
-  console.log(data.population)gotData;
+  bubblesort(data);
+  console.log(data.population);
 }
-function gotData(jData){
-   data = jData;
-}
+
 function draw(){
-  if(data);
+}
+
   function bubbleSort(){
-    for(var i = (data.length - 1); i >= 0; i--){
-      for(var j = 0; j < i; j++){
-        if(data[j].high < data[j+1].high){
+    var length = data.length
+    for(var i = 0; i < length; i++){
+      for(var j = 0; j < (length - i - 1); j++){
+        var a = data[j].population.high
+        var b = data[j+1].population.high
+        if(a > b){
           var temp = data[j]
-          data[j+1] = // TEMP: }
-     swap(j, j + 1)
-   }
+          data[j] = data[j+1]
+          data[j+1] = temp;
 
         }
       }
     }
   }
-}
